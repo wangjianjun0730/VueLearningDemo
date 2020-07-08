9c7befe720bf38c292e99b1b07840556fde6838e
wangjianjun <wangjianjun0730@126.com>
2020-07-08 22:09:39 +0800

    webpack

--- 31-模块化开发/模块化开发.js
+++ 31-模块化开发/模块化开发.js
@@ -1,1 +1,20 @@
-1.
+/**
+ * 1.为什么要有模块化
+ * 2.匿名闭包 解决命名冲突 但是 
+ * 3.不同的js文件 命名模块的名称导出，只需注意模块名称不重复即可
+ * */ 
+ var moudleA = (function(){
+     var obj = {};
+     obj.flag = true;
+     obj.myfunc = function(info){
+         console.log(info);
+     }
+     return obj;
+ })
+
+ /**
+  * 1.commonJS的模块化
+  * 2.ES6的模块化（导入和导出）
+  * 3.CMD模块化
+  * 4.AMD模块化
+ */
